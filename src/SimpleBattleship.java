014aaf68472d54a27217cb6ba4955b64ddbccd54
Daniel Sones <Daniel.sones@btinternet.com>
2020-05-01 19:09:53 +0100

    A few changes

--- src/SimpleBattleship.java
+++ src/SimpleBattleship.java
@@ -1,5 +1,5 @@
 
-public class SimpleDotCom {
+public class SimpleBattleship {
    int [] locationCells;
    int numOfHits = 0;
 
--- src/SimpleBattleshipsTestDrive.java
+++ src/SimpleBattleshipsTestDrive.java
@@ -1,23 +1,23 @@
 import java.util.Scanner;
 
-public class SimpleDotComTestDrive {
+public class SimpleBattleshipsTestDrive {
     public static void main (String[] args){
         // sets up scanner object
         Scanner reader = new Scanner(System.in);
         //set up some variables and create objects
         int numOfGuesses = 0;
         boolean isAlive = true;
-        SimpleDotCom theDotCom = new SimpleDotCom();
+        SimpleBattleship battleship = new SimpleBattleship();
         //Generate a random number that is equivilent to the cell
         int randomnum = (int) (Math.random() * 5);
         // fill in the random cell and plus one and then two
         int [] locations = {randomnum, randomnum + 1, randomnum + 2};
-        theDotCom.setLocationCells(locations);
+        battleship.setLocationCells(locations);
 
         while (isAlive == true){
             System.out.println("Where do you guess? (Potential positions start at zero)");
             String guess = reader.nextLine();
-            String result = theDotCom.checkYourself(guess);
+            String result = battleship.checkYourself(guess);
             numOfGuesses++;
             if (result.equals("kill")){
                 isAlive = false;
